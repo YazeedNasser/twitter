9fcb674b72d6c9708fb8ff3bef81c20bd70289d0
Yazeed <YazeedNasser0@gmail.com>
2018-12-12 12:29:22 +0200

    saved

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,7 +1,7 @@
 const graphql = require ( 'graphql' );
 const mutation = require ( './mutations/Mutate_Index' );
 const query = require ( './queries/Index_Query' );
-
+const passport = require('passport');
 const { GraphQLSchema } = graphql;
 
 module.exports = new GraphQLSchema({
